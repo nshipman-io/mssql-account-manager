7cbd532e8bfc733b25813b439daa4a7f7d1e7e9a
Norman Shipman <51459615+nshipman-io@users.noreply.github.com>
2019-06-18 15:53:16 -0400

    Update users.py

--- src/mssql/users.py
+++ src/mssql/users.py
@@ -5,7 +5,7 @@ from mssql import driver
 
 def randompassword():
   chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
-  size = random.randint(8, 12)
+  size = random.randint(12, 16)
   return ''.join(random.choice(chars) for x in range(size))
 
 def reset_password(connection,user_name):
@@ -16,7 +16,6 @@ def reset_password(connection,user_name):
         query=("""
         ALTER LOGIN %s WITH PASSWORD='%s' 
         """ % (user_name,user_password))
-        print(query)
         cursor.execute(query)
         connection.commit()
         print(f"The new password for the user is {user_password}")
